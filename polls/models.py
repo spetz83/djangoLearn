9f659e91132eb0b52f39f0931e35bb031b0a0fa9
Tom Etzel <tom.etzel@quantum.com>
2018-03-27 22:31:16 -0500

    UNIT TESTS!

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def wasPublishedRecently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Question
+
+
+def createQuestion(questionText, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=questionText, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+    def test_past_question(self):
+        createQuestion(questionText="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], ['<Question: Past question.>']
+        )
+
+
+    def test_future_question(self):
+        createQuestion(questionText='Future question.', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+    def test_future_question_and_past_question(self):
+        createQuestion(questionText="Past question.", days=-30)
+        createQuestion(questionText="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], ['<Question: Past question.>']
+        )
+
+
+    def test_two_past_questions(self):
+        createQuestion(questionText="Past question 1.", days=-30)
+        createQuestion(questionText="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        futureQuestion = createQuestion(questionText="Future question.", days=5)
+        url = reverse('polls:detail', args=(futureQuestion.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_past_question(self):
+        pastQuestion = createQuestion(questionText="Past question.", days=-5)
+        url = reverse('polls:detail', args=(pastQuestion.id,))
+        response = self.client.get(url)
+        self.assertContains(response, pastQuestion.question_text)
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        futureQuestion = Question(pub_date=time)
+        self.assertIs(futureQuestion.wasPublishedRecently(), False)
+
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        oldQuestion = Question(pub_date=time)
+        self.assertIs(oldQuestion.wasPublishedRecently(), False)
+
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recentQuestion = Question(pub_date=time)
+        self.assertIs(recentQuestion.wasPublishedRecently(), True)
+
